f387796606f2f4b3bf6f29191d3eedb097e6ed0c
moczniak <czornypan@gmail.com>
2019-11-03 18:42:59 +0100

    export url

--- angular_router/lib/angular_router.dart
+++ angular_router/lib/angular_router.dart
@@ -20,3 +20,4 @@ export 'src/router/navigation_params.dart' show NavigationParams;
 export 'src/router/router.dart' show Router, NavigationResult;
 export 'src/router/router_state.dart' show RouterState;
 export 'src/router_hook.dart' show RouterHook;
+export 'src/url.dart';
